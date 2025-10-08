70f355a7f5fa091338bd51d3e65cdd5fb232ca03
A Alvarez Etchegaray <alejandro.alvarez@kiongroup.com>
2025-10-08 08:55:10 +0200

    In case access_token does not exist.

--- lib/streamlit/web/server/oauth_authlib_routes.py
+++ lib/streamlit/web/server/oauth_authlib_routes.py
@@ -179,6 +179,7 @@ class AuthCallbackHandler(AuthHandlerMixin, tornado.web.RequestHandler):
         user = cast("dict[str, Any]", token.get("userinfo"))
 
         cookie_value = dict(user, origin=origin, is_logged_in=True)
+        tokens = {}
         if "access_token" in token:
             tokens = {"access_token": token["access_token"]}
 
